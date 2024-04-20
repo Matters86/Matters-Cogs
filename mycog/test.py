df0ba1492419bddaa994e6a467ea472e0346d015
Thomas Göttsching <support@thomasgoettsching.de>
2024-04-20 12:37:43 +0200

    API Abruf im test

--- mycog/test.py
+++ mycog/test.py
@@ -2,13 +2,33 @@ import aiohttp
 import asyncio
 import json
 
-async def push():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.restful-api.dev/objects') as response:
-            html = await response.text()
-            myObject = json.loads(html)
-            
-            print(myObject[1]['name'])
-            
+personalToken = '' #Personal Token
+headers = {'Authorization': 'Bearer ' + personalToken}
 
-asyncio.run(push())
+async def call_api(url, headers, params):
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(url=url, params=params) as response:
+            resp = await response.read()
+            return resp
+
+async def getCompanies(headers):
+    url = 'https://api.statev.de/req/factory/list'
+    resp = await call_api(url, headers, {})
+    companiesObj = json.loads(resp)
+    return companiesObj
+
+async def getCompanyInventory(headers, companyID):
+    url = 'https://api.statev.de/req/factory/inventory/' + companyID
+    resp = await call_api(url, headers, {})
+    inventoryObj = json.loads(resp)
+    return inventoryObj
+
+
+companies = asyncio.run(getCompanies(headers))
+if ( companies and len(companies) > 0 ):
+    for company in companies:
+        companyID = company['id']
+        inventory = asyncio.run(getCompanyInventory(headers, companyID))
+        print(inventory)
+
+#asyncio.run(push())
